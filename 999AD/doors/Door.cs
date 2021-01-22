0dc5997fb57fb1095c12fd032581a80539db9e1d
Ivan Palazzo <w1744272@westminster.ac.uk>
2021-01-22 19:37:00 +0000

    credits

--- 999AD/doors/Door.cs
+++ 999AD/doors/Door.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using System.CodeDom;
 
 namespace _999AD
 {
@@ -9,14 +8,15 @@ namespace _999AD
         #region DECLARATIONS
         public enum TextureType
         {
-            brassDoor, goldDoor,bronzeDoor, silverDoor,  total
+            brassDoor, goldDoor, bronzeDoor, silverDoor,  total
         }
         static public int IDcounter;
         public int ID { get; private set; } //used to keep track of doors that have beeen opened
         static Texture2D spritesheet;
+        //each source rectangle define the position of the image of the door in the spritesheet
         static Rectangle[] sourceRectangles;
-        Point position;
-        Collectable.ItemType key;
+        Point position;//position of the door in world coordinates
+        Collectable.ItemType key;//type of collectable that opens the door
         TextureType textureType;
         bool closed;
         #endregion
@@ -33,6 +33,7 @@ namespace _999AD
         public static void Inizialize(Texture2D _spritesheet)
         {
             spritesheet = _spritesheet;
+            //define the source rectangle for each door type
             sourceRectangles = new Rectangle[(int)TextureType.total]
                 {
                     new Rectangle(0,0,16,40),
@@ -43,6 +44,8 @@ namespace _999AD
         }
         #endregion
         #region PROPERTIES
+
+        //the player can open the door if they are inside this rectangle
         Rectangle InteractionRectangle
         {
             get
@@ -53,6 +56,8 @@ namespace _999AD
                                     sourceRectangles[(int)textureType].Height);
             }
         }
+
+        //rectangle within which the door is drawn
         Rectangle DrawRectangle
         {
             get
@@ -63,15 +68,17 @@ namespace _999AD
                                     sourceRectangles[(int)textureType].Height);
             }
         }
+
         public bool Closed
         {
             get { return closed; }
         }
         #endregion
         #region METHODS
+
         public void LockDoor(RoomsManager.Rooms room)
         {
-            //add non traversabe tiles on top of the door
+            //make all the tiles on top of the door to type "solidEmpty" (invisible and NOT traversable)
             int topRow = (int)position.Y / Tile.tileSize;
             int btmRow = ((int)position.Y + sourceRectangles[(int)textureType].Height - 1) / Tile.tileSize;
             int leftCol = (int)position.X / Tile.tileSize;
@@ -97,9 +104,9 @@ namespace _999AD
             }
         }
 
-        //remove door
         public void OpenDoor(int roomType)
         {
+            //turn all the tiles on top of the door to type "empty" (invisible and traversable)
             closed = false;
             int topRow = (int)position.Y / Tile.tileSize;
             int btmRow = ((int)position.Y + sourceRectangles[(int)textureType].Height - 1) / Tile.tileSize;
